bc0a5f3bf6c6d768177c0b1b2e312487c9d0e1ab
daniel salamanca <sebassala1992@hotmail.es>
2020-07-27 12:16:27 -0500

    primer programa clase de Python conversor de dolares

---
+++ conversor.py
@@ -0,0 +1,7 @@
+pesos = input("¿cuántos pesos Colombianos tirns?:  ")
+pesos = float(pesos)
+valor_dolar = 3687
+dolares = pesos / valor_dolar
+dolares = str(dolares)
+print("Tienes $" + dolares +" dólares" ) 
+
