6573245340cc7c3a67d5dbb7661233121dcad0cf
daniel salamanca <sebassala1992@hotmail.es>
2020-07-27 15:45:09 -0500

    Primera versión solo trasforma peso colombianos a dólares

--- conversor.py
+++ conversor.py
@@ -2,6 +2,7 @@ pesos = input("¿cuántos pesos Colombianos tirns?:  ")
 pesos = float(pesos)
 valor_dolar = 3687
 dolares = pesos / valor_dolar
+dolares = round(dolares, 2)
 dolares = str(dolares)
 print("Tienes $" + dolares +" dólares" ) 
 
