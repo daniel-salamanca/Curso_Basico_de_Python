5edd64a0a2eb366c2da7a4ad04eada7a9866ee02
daniel salamanca <sebassala1992@hotmail.es>
2020-07-28 14:22:04 -0500

    Se realizan cambios a la versión 0.1 agregando el menú de moneda

--- conversor.py
+++ conversor.py
@@ -1,8 +1,29 @@
-pesos = input("¿cuántos pesos Colombianos tirns?:  ")
-pesos = float(pesos)
-valor_dolar = 3687
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes $" + dolares +" dólares" ) 
+menu = """
+Bienbenido al conversor de monedas 💰
+
+1 - Pesos Colombianos
+2 - Pesos Argentinos
+3 - Pesos Eexicanos
+4 - Euros 
+
+Elige una opción"""
+
+opcion =int(input(menu))
+
+if opcion == 1:
+    pesos = input("¿cuántos pesos Colombianos tirns?:  ")
+    pesos = float(pesos)
+    valor_dolar = 3687
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares +" dólares" ) 
+elif opcion == 2:
+    pass
+elif opcion == 3:
+    pass
+elif opcion == 4
+    pass
+else:
+    print("Ingresa una opción correcta por favor")
 
