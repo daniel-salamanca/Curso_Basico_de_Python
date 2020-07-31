5f4b9d7251025b6d4c9bd8d2f657832b4fed17a1
daniel salamanca <sebassala1992@hotmail.es>
2020-07-31 15:47:23 -0500

    Se agrega ejercicio de bucles con la sentecia for

---
+++ for.py
@@ -0,0 +1,20 @@
+#for contador in range(1001):
+#    print(contador)
+
+
+# for i in range(11):
+#     print( "8 por "+str(i)+" es igual a " +str(8*i ))
+
+def  run():
+    # nombre = input('Escribre tu nombre: ')
+    # for letra in nombre:
+    #     print(letra) 
+    
+    frase = input("Escribe un a frase: ")
+    for caracter in frase:
+        print(caracter.upper())
+
+if __name__ =="__main__":
+    run()
+
+ 
